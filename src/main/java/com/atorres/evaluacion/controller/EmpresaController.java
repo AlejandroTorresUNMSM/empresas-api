3f49af0a4a73f2ac697237f42855708b979c9188
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-05-13 15:33:27 -0500

    dev: controller

--- src/main/java/com/atorres/evaluacion/controller/EmpresaController.java
+++ src/main/java/com/atorres/evaluacion/controller/EmpresaController.java
@@ -9,8 +9,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
